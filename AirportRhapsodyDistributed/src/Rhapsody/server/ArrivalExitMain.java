7f8c8675cf8a076808b360087bf7b2b22feb93d3
Ze Paiva <zmppaiva99@gmail.com>
2020-06-05 21:54:10 +0100

    added 2 mains, someone can follow teh example now

--- AirportRhapsodyDistributed/src/Rhapsody/server/ArrivalExitMain.java
+++ AirportRhapsodyDistributed/src/Rhapsody/server/ArrivalExitMain.java
@@ -1,5 +1,76 @@
 package Rhapsody.server;
 
+import Rhapsody.server.stubs.DepartureEntranceStub;
+
+import java.net.SocketTimeoutException;
+
+import Rhapsody.common.RunParameters;
+import Rhapsody.server.communications.ServerCom;
+import Rhapsody.server.communications.TunnelProvider;
+import Rhapsody.server.proxies.ArrivalExitProxy;
+import Rhapsody.server.sharedRegions.ArrivalExit;
+import Rhapsody.server.stubs.ArrivalLoungeStub;
+import Rhapsody.server.stubs.ArrivalQuayStub;
+import Rhapsody.server.stubs.GeneralRepositoryStub;
+
+/**
+ * Main class for the Arrival Terminal Exit Shared memmory Launches the Java VM
+ * for this entity only
+ * 
+ * @author José Paiva
+ * @author André Mourato
+ */
 public class ArrivalExitMain {
-    
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Create communication utilities
+         */
+        ServerCom serverCommunications, serverConnections;
+        TunnelProvider provider;
+
+        /**
+         * Create stubs for cross comunication
+         */
+        GeneralRepositoryStub repository = new GeneralRepositoryStub();
+        ArrivalQuayStub arrivalQuay = new ArrivalQuayStub();
+        DepartureEntranceStub departureEntrance = new DepartureEntranceStub();
+        ArrivalLoungeStub arrivalLounge = new ArrivalLoungeStub();
+
+        /**
+         * Create main entity
+         */
+        ArrivalExit arrivalExit = new ArrivalExit(repository, arrivalQuay, departureEntrance, arrivalLounge);
+
+        /**
+         * Create main entity proxy
+         */
+        ArrivalExitProxy arrivalExitProxy = new ArrivalExitProxy(arrivalExit);
+
+        /**
+         * Start comms
+         */
+        serverCommunications = new ServerCom(RunParameters.ArrivalExitPort, 1000);
+        serverCommunications.start();
+
+        while (!arrivalExitProxy.hasSimEnded()) {
+            try {
+                serverConnections = serverCommunications.accept();
+                provider = new TunnelProvider(arrivalExitProxy, serverConnections);
+                provider.start();
+            } catch (SocketTimeoutException e) {
+                System.err.println("Socket timeout on arrival exit");
+                e.printStackTrace();
+            } catch (RuntimeException e) {
+                System.err.println("Error on proxy");
+                e.printStackTrace();
+            }                
+        }
+        System.out.println("[Arrival Terminal Exit] terminating...");
+    }
 }
--- AirportRhapsodyDistributed/src/Rhapsody/server/ArrivalLoungeMain.java
+++ AirportRhapsodyDistributed/src/Rhapsody/server/ArrivalLoungeMain.java
@@ -1,5 +1,71 @@
 package Rhapsody.server;
 
+import java.net.SocketTimeoutException;
+
+import Rhapsody.common.RunParameters;
+import Rhapsody.server.communications.ServerCom;
+import Rhapsody.server.communications.TunnelProvider;
+import Rhapsody.server.proxies.ArrivalLoungeProxy;
+import Rhapsody.server.sharedRegions.ArrivalLounge;
+import Rhapsody.server.stubs.BaggageCollectionStub;
+import Rhapsody.server.stubs.GeneralRepositoryStub;
+
+/**
+ * Main class for the arrival lounge entity
+ * 
+ * @author José Paiva
+ * @author André Mourato
+ */
 public class ArrivalLoungeMain {
     
+    /**
+     * Main method 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        /**
+         * Create communication utilities
+         */
+        ServerCom serverCommunication, serverConnections;
+        TunnelProvider provider;
+
+        /**
+         * Create stubs
+         */
+        GeneralRepositoryStub repository = new GeneralRepositoryStub();
+        BaggageCollectionStub baggageCollection = new BaggageCollectionStub();
+
+        /**
+         * Create main entity
+         */
+        ArrivalLounge arrivalLounge = new ArrivalLounge(repository, baggageCollection);
+
+        /**
+         * Create proxy
+         */
+        ArrivalLoungeProxy arrivalLoungeProxy = new ArrivalLoungeProxy(arrivalLounge);
+
+        /**
+         * Start comms
+         */
+        serverCommunication = new ServerCom(RunParameters.ArrivalLoungePort, 1000);
+        serverCommunication.start();
+
+        while (!arrivalLoungeProxy.hasSimEnded()) {
+            try {
+                serverConnections = serverCommunication.accept();
+                provider = new TunnelProvider(arrivalLoungeProxy, serverConnections);
+                provider.start();
+            } catch (SocketTimeoutException e) {
+                System.err.println("Socket timeout on arrival exit");
+                e.printStackTrace();
+            } catch (RuntimeException e) {
+                System.err.println("Error on proxy");
+                e.printStackTrace();
+            }     
+        }
+
+        System.out.println("[Arrival Lounge] terminating...");
+    }
 }
